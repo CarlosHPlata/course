f8701075e55f14dc107fb235ce558185b2113581
Alexandra Oreza <A18016321@alumnos.uady.mx>
2022-09-13 11:54:53 -0500

    OO tdd Bowling example

--- inclass/src/1_5_tdd/objectOriented/bowling.spec.ts
+++ inclass/src/1_5_tdd/objectOriented/bowling.spec.ts
@@ -2,10 +2,52 @@ import { BowlingGame } from "./bowling";
 
 
 describe('A bowling game', () => {
-    
+  
+  beforeEach
   let game:BowlingGame
   beforeEach(() => {
     game = new BowlingGame()
   });
 
+  it('can roll a gutter game', () =>{
+// roll all 0s
+    RollMany(20, 0)
+    expect(game.Score()).toBe(0)
+  })
+
+  it('can roll all 1s', () =>{
+    RollMany(20, 1)
+    expect(game.Score()).toBe(20)
+  })
+
+  it('can roll a spare', () =>{
+    game.Roll(5)
+    game.Roll(5)
+    game.Roll(3)
+    RollMany(17, 0)
+
+    expect(game.Score()).toBe(16)
+  })
+
+  it('can roll a Strike', () =>{
+    game.Roll(10)
+    game.Roll(1)
+    game.Roll(1)
+    RollMany(16,0)
+
+    expect(game.Score()).toBe(14)
+  })
+
+  it('can roll a perfect game', () =>{
+  //roll all 10s
+    RollMany(12, 10)
+    
+    expect(game.Score()).toBe(300)
+  })
+
+  function RollMany(rolls:number, pins:number){
+    for (let i = 0; i < rolls; i++) {
+      game.Roll(pins); 
+    }
+  }
 });
--- inclass/src/1_5_tdd/objectOriented/bowling.ts
+++ inclass/src/1_5_tdd/objectOriented/bowling.ts
@@ -1,5 +1,51 @@
 export class BowlingGame {
+    private rolls: number[]
+    score: number
 
+    constructor(){
+        this.rolls = []
+        this.score = 0
+    }
 
-  
+    Roll(pins:number){
+        this.rolls.push(pins);
+    }
+
+    Score(){
+        let RollIndex = 0
+        for (let i = 0; i < 10; i++) {
+            if(isStrike(this.rolls, i)){
+                this.score += 10 + StrikeBonus(this.rolls, RollIndex)
+                RollIndex++
+            }else if(isSpare(this.rolls, i)){
+                this.score += 10 + SpareBonus(this.rolls, RollIndex)
+                RollIndex += 2
+            }else{
+                this.score += BallsInFrame(this.rolls, RollIndex)
+                RollIndex += 2
+            }         
+        }
+        return this.score
+    }
+    
+}
+
+const BallsInFrame = (rolls: number[], firstinFrame: number) =>{
+    return rolls[firstinFrame] + rolls[firstinFrame + 1]
+}
+
+function isSpare(rolls: number[], frameIndex:number) {
+    return rolls[frameIndex] + rolls[frameIndex + 1] == 10
+}
+
+const SpareBonus = (rolls: number[], firstinFrame: number) =>{
+    return rolls[firstinFrame + 2]
+}
+
+function isStrike(rolls: number[], frameIndex:number){
+    return rolls[frameIndex] == 10
+}
+
+const StrikeBonus = (rolls: number[], firstinFrame: number) =>{
+    return rolls[firstinFrame + 1] + rolls[firstinFrame + 2]
 }
