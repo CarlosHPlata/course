4bc9f9ee9080e779a1ab359569f8a4594986b067
Carlos Plata <carlos.plata@rappi.com>
2023-08-22 17:24:27 +0200

    feat: clean code before

---
+++ inclass/src/1_CleanCode/before/1.namingRules.ts
@@ -0,0 +1,21 @@
+function makeOrders(x) {
+  for (let i = 0; i < x.length; i++) {
+    const z = x[i]
+
+    for (let j = i + 1; j < x.length; j++) {
+      const y = x[j]
+      if (z.price - y.price <= 327) {
+        z.related.push(y.id)
+      }
+    }
+  }
+
+  return x
+}
+
+console.log(
+  makeOrders([
+    { id: 1, price: 1000, related: [] },
+    { id: 2, price: 1001, related: [] },
+  ])
+)
---
+++ inclass/src/1_CleanCode/before/4.classesAndDs.ts
@@ -0,0 +1,42 @@
+const pokemon = {
+  id: 1,
+  speed: 20,
+  isGiga: false,
+  isMega: true,
+  isLegendary: true,
+}
+const trainer = { getPc: () => ({ getPCSlots: () => [{ pokemon }] }) }
+
+class Attack {
+  public readonly BASE_ATTACK = 20
+  public attackSpeed: number
+
+  constructor(pokemonSpeed: number) {
+    this.attackSpeed = pokemonSpeed * 2
+  }
+
+  public getDamage(type) {
+    switch (type) {
+      case 'NORMAL':
+        return this.attackSpeed * this.BASE_ATTACK
+      case 'MEGA':
+        return this.attackSpeed * 40 * this.BASE_ATTACK
+      case 'GIGAMAX':
+        return this.attackSpeed + this.BASE_ATTACK < 100
+          ? this.attackSpeed * this.BASE_ATTACK + 100
+          : this.BASE_ATTACK
+    }
+  }
+}
+
+const performAttack = (trainer, pokemonId) => {
+  const pokemon = trainer
+    .getPc()
+    .getPCSlots()
+    .find((slot) => slot.pokemon.id === pokemonId).pokemon
+
+  const attack = new Attack(pokemon.speed)
+  return attack.attackSpeed * attack.BASE_ATTACK
+}
+
+console.log(performAttack(trainer, 1))
