f6a7e26e130b308b87a307d22bf4e44c77d1bb60
Carlos Plata <carlos.plata@rappi.com>
2023-08-24 20:39:47 +0200

    feat: big o excercises

---
+++ inclass/src/2_BigO/algorithm5.ts
@@ -0,0 +1,32 @@
+/* 
+  PLEASE WRITE YOUR ANSWERS IN THE ANSWER CONSTANT
+  TO CHECK IF YOUR ANSWERS ARE CORRECT RUN 
+  npm run practice -- bigO
+
+    - ALWAYS IN FORMAT O(answer)
+    - TO USE THE POWER SYMBOL WRITE ^ LIKE IF YOU WANT O(2^n)
+    - LOGARARITHMS WRITE WITH log LIKE O(log n) or O(log (n))
+    - MULTIPLICATIONS DON'T HAVE SYMBOL SO IF YOU WANT TO EXPRESS A*B JUST WRITE O(AB) or O(A B)
+*/
+
+// ----- YOUR ANSWERS ------
+//This algorithm is a very famous one.
+
+export const ANSWER_E = 'O(1)'
+export const EXTRA_POINT1 = 'what algorythm is'
+
+// ------------------------
+
+export const whatIsThis = (arr: number[], x: number) => {
+  let start = 0
+  let end = arr.length - 1
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2)
+
+    if (arr[mid] === x) return true
+    else if (arr[mid] < x) start = mid + 1
+    else end = mid - 1
+  }
+  return false
+}
---
+++ inclass/src/2_BigO/algorithm6.ts
@@ -0,0 +1,32 @@
+/* 
+  PLEASE WRITE YOUR ANSWERS IN THE ANSWER CONSTANT
+  TO CHECK IF YOUR ANSWERS ARE CORRECT RUN 
+  npm run practice -- bigO
+
+    - ALWAYS IN FORMAT O(answer)
+    - TO USE THE POWER SYMBOL WRITE ^ LIKE IF YOU WANT O(2^n)
+    - LOGARARITHMS WRITE WITH log LIKE O(log n) or O(log (n))
+    - MULTIPLICATIONS DON'T HAVE SYMBOL SO IF YOU WANT TO EXPRESS A*B JUST WRITE O(AB) or O(A B)
+*/
+
+// ----- YOUR ANSWERS ------
+
+export const ANSWER_F = 'O(1)'
+
+// ------------------------
+
+export const allFib = (n: number) => {
+  const memo = {}
+  for (let i = 0; i < n; i++) {
+    fibWithMemo(i, memo)
+  }
+}
+
+const fibWithMemo = (n: number, memo: Record<number, number>) => {
+  if (n <= 0) return 0
+  else if (n == 1) return 1
+  else if (memo[n] === undefined)
+    memo[n] = fibWithMemo(n - 1, memo) + fibWithMemo(n - 2, memo)
+
+  return memo[n]
+}
