fcd199a7e06d6e80d647fdc4d232d4e8ebc56149
Carlos Plata <carlos.plata@rappi.com>
2023-09-12 19:51:09 +0200

    feat: fizz buzz practice

---
+++ inclass/src/4_Dojo/fizzBuzzBar/fizzBuzzBar.spec.ts
@@ -0,0 +1,5 @@
+describe('Test for fizz buzz bar', () => {
+  it('should ', () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ inclass/src/4_Dojo/fizzBuzzBar/fizzBuzzBar.ts
@@ -0,0 +1,10 @@
+// escribir un codigo que reciba un numero entero
+// si el numero es multiplo de 3 devolver fizz
+// si el numero es multiplo de 5 devolver buzz
+// si el numero es multiplo de 3 y 5 devolver fizzbuzz
+// si el numero es multiplo de 7 devolver bar
+// si el numero es multiplo de 7 y 3 fizzbar
+// si el numero es multiplo de 7 y 5 buzzbar
+// si el numero es multiplo de 7, 3 y 5 devolver fizzbuzzbar
+// pensar en diseno para seguir anadiendo numeros como 13 y foo
+// si el numero no es multiplo de ninguno devolver solo el numero.
