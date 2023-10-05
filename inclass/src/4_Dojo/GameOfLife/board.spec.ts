31001787c408357ba621e52c38e5245353bcbe3d
Carlos Plata <carlos.plata@rappi.com>
2023-10-05 23:10:38 +0200

    feat: game of life part 1

--- inclass/src/4_Dojo/GameOfLife/board.spec.ts
+++ inclass/src/4_Dojo/GameOfLife/board.spec.ts
@@ -1,3 +1,50 @@
+import { createBoard } from "./board";
+import { Cell } from "./models/Cell";
+
+
 describe('Board testing', () => {
 
+  let board: ReturnType<typeof createBoard>
+  beforeEach(() => {
+    board = createBoard()
+  })
+
+  it("create cells", () => {
+
+    const cell: Cell = [1, 2];
+    board.addCell(cell);
+    const isLive: boolean = board.isAlive(cell);
+
+    expect(isLive).toBeTruthy()
+  })
+
+  it('should check if a cell is dead ', () => {
+    const cell: Cell = [1, 2];
+    board.addCell(cell);
+    const isLive: boolean = board.isAlive([2, 3]);
+
+    expect(isLive).toBeFalsy()
+  });
+
+  it('should get live cell neighbours ', () => {
+    const cell: Cell = [1, 2];
+    const cell_neighbour: Cell = [1, 3];
+
+    board.addCell(cell);
+    board.addCell(cell_neighbour);
+
+    const have_neighbour: boolean = board.getNeighbours(cell).has(cell_neighbour);
+    expect(have_neighbour).toBeTruthy()
+  });
+
+  it('shouldnt find not neighbour cells', () => {
+    const cell: Cell = [1, 2];
+    const not_neighbour: Cell = [9, 9];
+
+    board.addCell(cell);
+    board.addCell(not_neighbour);
+
+    const have_neighbour: boolean = board.getNeighbours(cell).has(not_neighbour);
+    expect(have_neighbour).toBeFalsy()
+  });
 });
--- inclass/src/4_Dojo/GameOfLife/board.ts
+++ inclass/src/4_Dojo/GameOfLife/board.ts
@@ -1,2 +1,35 @@
 import { Cell } from "./models/Cell"
 import { CellSet } from "./models/CellSet"
+
+
+export function createBoard() {
+  let cells: Set<Cell> = new CellSet()
+
+  return {
+    addCell: (cell: Cell) => addCell(cells, cell),
+    isAlive: (cell: Cell) => isAlive(cells, cell),
+    getNeighbours: (cell: Cell) => getNeighbours(cells, cell)
+  };
+}
+
+const addCell = (cells: Set<Cell>, cell: Cell) => {
+  cells.add(cell);
+}
+
+const isAlive = (cells: Set<Cell>, cell: Cell) => {
+  return cells.has(cell);
+}
+
+const getNeighbours = (cells: Set<Cell>, cell: Cell) => {
+  const [x, y] = cell
+  let neighbours: Set<Cell> = new CellSet()
+
+  for (let i = -1; i <= 1; i++) {
+    for (let k = -1; k <= 1; k++) {
+      neighbours.add([x + i, y + k])
+    }
+  }
+
+  return neighbours;
+}
+//
