3c54979cf58d9316c7efc48b4fe87c1bfecc19fc
CarlosJCD <carlos.calderon.d@icloud.com>
2024-01-26 12:54:51 -0600

    Responses BigO

--- inclass/src/2_BigO/algorithm1.ts
+++ inclass/src/2_BigO/algorithm1.ts
@@ -5,13 +5,13 @@
 
     - ALWAYS IN FORMAT O(answer)
     - TO USE THE POWER SYMBOL WRITE ^ LIKE IF YOU WANT O(2^n)
-    - LOGARARITHMS WRITE WITH log LIKE O(log n) or O(log (n))
+    - LOGARITHMS WRITE WITH log LIKE O(log n) or O(log (n))
     - MULTIPLICATIONS DON'T HAVE SYMBOL SO IF YOU WANT TO EXPRESS A*B JUST WRITE O(AB) or O(A B)
 */
 
 // ----- YOUR ANSWERS ------
 
-export const ANSWER_A = 'O(1)'
+export const ANSWER_A = 'O(2^n)'
 
 // ------------------------
 
