10bdd739ca7854a05e418024b2144cdb8124df26
Jonatan <a17000799@alumnos.uayd.mx>
2024-02-01 10:29:02 -0600

    feat: TDD excercises

--- inclass/src/3_TDD/functional/bowlingGame.spec.ts
+++ inclass/src/3_TDD/functional/bowlingGame.spec.ts
@@ -1,8 +1,55 @@
-
+import {Game, createBowlingGame} from "./bowlingGame"
 describe('Test bowling game', () => {
-
-  it('dummy test', () => {
-    expect(1).toBe(1)
+  let game : Game;
+  beforeEach(()=>{
+    game = createBowlingGame()
   })
 
+  it('Should play a gutter game', () => {
+    game = rollMany(game, 20, 0)
+    expect( game.score() ).toBe( 0 )
+  })
+
+  it('Should roll all ones', () => {
+    game = rollMany(game, 20, 1)
+    expect( game.score() ).toBe( 20 )
+  });
+
+  it('Should roll a spare', () => {
+    game = rollSpare(game) 
+          .roll(3) 
+    game = rollMany(game, 17, 0)
+    expect( game.score() ).toBe(16)
+  });
+
+  it('Should roll a strike', () => {
+    game = rollStrike(game)
+      .roll(3)
+      .roll(3)
+    game = rollMany(game, 16, 0)
+    expect(game.score()).toBe(22)
+  });
+
+  it('Should do a perfect game', () => {
+    game = rollMany(game, 12, 10)
+    expect(game.score()).toBe(300)
+  });
+
 });
+
+function rollStrike(game: Game) {
+  return game.roll(10);
+}
+
+function rollSpare(game: Game) {
+  return game
+    .roll(5)
+    .roll(5);
+}
+
+function rollMany(game: Game, rolls: number, pins: number) {
+  for (let i = 0; i < rolls; i++) {
+    game = game.roll(pins);
+  }
+  return game;
+}
--- inclass/src/3_TDD/functional/bowlingGame.ts
+++ inclass/src/3_TDD/functional/bowlingGame.ts
@@ -1,22 +1,53 @@
-/* Remember that all your functions have to respect the functional programming rules:
- *  - 🔥 Pure functions and side effects 
- *  - 🔥 First class functions & higher order functions
- *  - 🔥 Immutability ⚠️
- *  - 🔥 Composition 
- *
- * See more info here: 
- * https://www.learningjournal.guru/courses/scala/scala-programming-foundation/elements-of-functional-programming-1/
- * 
- * I'm putting a warning in Immutability to remember you that every time you try
- * to change the state var... just be carefull...⚠️
- */
-
-type State = { /* put params if needed */ }
-const INITIAL_STATE: State = {}
+type State = { rolls: number[] } 
+const INITIAL_STATE: State = { rolls: [] }  
 
 export const createBowlingGame = (state: State = INITIAL_STATE) => Object.freeze({
-  //myFunction: () => callInnerModuleScopeFunction()
-  //myOhterFunction: (...) => myOtherFunction(...)
+  roll : (pins: number) => roll(state, pins),
+  score: () => score(state)
 })
 
-//const myOtherFunction = (...) => {...}
+export type Game = ReturnType<typeof createBowlingGame>
+
+const roll = (state: State, pins:number) => {
+  return createBowlingGame({ rolls: [...state.rolls, pins] })
+}
+          
+const score = ({ rolls }: State) => {
+  const FRAMES = 10
+  let score = 0
+  let firstTry = 0
+
+  for(let frame = 0; frame < FRAMES; frame++){
+    if(isStrike(rolls, firstTry)){
+      score += scoreForStrike(rolls, firstTry)
+      firstTry += 1
+    }else if (isSpare(rolls, firstTry)){
+      score += scoreForSpare(rolls, firstTry)
+      firstTry += 2
+    }else{
+      score += scoreForFrame(rolls, firstTry)
+      firstTry += 2
+    }
+  }
+  return score
+}
+
+function scoreForStrike(rolls: number[],firstTry:number){
+  return 10 + rolls[firstTry+1] + rolls[firstTry + 2]
+}
+
+function isStrike(rolls: number[], firstTry:number){
+  return rolls[firstTry] == 10
+}
+
+function scoreForSpare(rolls: number[],firstTry:number){
+  return 10 + rolls[firstTry + 2]
+}
+
+function isSpare(rolls: number[], firstTry:number){
+  return rolls[firstTry] + rolls[firstTry + 1] == 10
+}
+
+function scoreForFrame(rolls: number[],firstTry:number){
+  return rolls[firstTry] + rolls[firstTry + 1]
+}
