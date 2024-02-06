683830683f2c9920a3a82b8d113f0d8d7b7d1f84
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-05 18:53:55 -0600

    TDD Bowling Kata functional programming

--- inclass/src/3_TDD/functional/bowlingGame.spec.ts
+++ inclass/src/3_TDD/functional/bowlingGame.spec.ts
@@ -1,8 +1,57 @@
+import { createBowlingGame } from "./bowlingGame";
 
-describe('Test bowling game', () => {
+describe("Test bowling game", () => {
+    let bowlingGame = createBowlingGame();
 
-  it('dummy test', () => {
-    expect(1).toBe(1)
-  })
+    beforeEach(() => (bowlingGame = createBowlingGame()));
 
+    it("should roll a gutter game", () => {
+        bowlingGame = rollMany(20, 0);
+
+        expect(bowlingGame.getFinalScore()).toBe(0);
+    });
+
+    it("should roll a game of 1", () => {
+        bowlingGame = rollMany(20, 1);
+
+        expect(bowlingGame.getFinalScore()).toBe(20);
+    });
+
+    it("should roll a spare", () => {
+        bowlingGame = bowlingGame.roll(5).roll(5).roll(4);
+
+        bowlingGame = rollMany(17, 0);
+
+        expect(bowlingGame.getFinalScore()).toBe(18);
+    });
+
+    it("should roll a strike",()=>{
+      bowlingGame = bowlingGame.roll(10).roll(5).roll(4);
+
+      bowlingGame = rollMany(17,0);
+
+      expect(bowlingGame.getFinalScore()).toBe(28);
+
+    })
+
+    it("should do a perfect game",()=>{
+        bowlingGame = rollMany(12, 10);
+
+        expect(bowlingGame.getFinalScore()).toBe(300);
+    })
+
+    it("should do a spare on last frame",()=>{
+        bowlingGame = rollMany(18, 0)
+
+        bowlingGame = bowlingGame.roll(5).roll(5).roll(4)
+
+        expect(bowlingGame.getFinalScore()).toBe(14)
+    })
+
+    function rollMany(totalRolls: number, pinsPerRoll: number) {
+        for (let i = 0; i < totalRolls; i++) {
+            bowlingGame = bowlingGame.roll(pinsPerRoll);
+        }
+        return bowlingGame;
+    }
 });
--- inclass/src/3_TDD/functional/bowlingGame.ts
+++ inclass/src/3_TDD/functional/bowlingGame.ts
@@ -8,15 +8,74 @@
  * https://www.learningjournal.guru/courses/scala/scala-programming-foundation/elements-of-functional-programming-1/
  * 
  * I'm putting a warning in Immutability to remember you that every time you try
- * to change the state var... just be carefull...⚠️
+ * to change the state var... just be careful...⚠️
  */
 
-type State = { /* put params if needed */ }
-const INITIAL_STATE: State = {}
+type GameState = { rolls: any[]}
+const INITIAL_GAME_STATE: GameState = {rolls: []}
 
-export const createBowlingGame = (state: State = INITIAL_STATE) => Object.freeze({
-  //myFunction: () => callInnerModuleScopeFunction()
-  //myOhterFunction: (...) => myOtherFunction(...)
+const CURRENT_FRAME_FIRST_ROLL_INDEX = 0;
+const CURRENT_FRAME_SECOND_ROLL_INDEX = 1;
+
+const NEXT_FRAME_FIRST_ROLL_INDEX = CURRENT_FRAME_SECOND_ROLL_INDEX + 1;
+const NEXT_FRAME_SECOND_ROLL_INDEX = CURRENT_FRAME_SECOND_ROLL_INDEX + 2;
+
+const STRIKE_ROLL_SCORE = 10;
+
+export const createBowlingGame = (gameState: GameState = INITIAL_GAME_STATE) => Object.freeze({
+
+  roll: (pinsRolled:number) => {
+    return createBowlingGame({rolls: [...gameState.rolls, pinsRolled]})
+  },
+
+  getFinalScore: (totalRolls: any[] = gameState.rolls)=>{
+    
+    if(totalRolls.length == 0) return 0;
+
+    if(totalRolls.length == 1) return totalRolls[CURRENT_FRAME_FIRST_ROLL_INDEX];
+
+    const firstRoll: number = totalRolls[CURRENT_FRAME_FIRST_ROLL_INDEX];
+
+    if(isStrike(firstRoll)){
+      let strikeFrameScore = calculateStrikeFrameScore(totalRolls)
+
+      if(isLastFrame(totalRolls))  return strikeFrameScore;
+
+      return  strikeFrameScore + createBowlingGame({rolls: totalRolls.slice(CURRENT_FRAME_SECOND_ROLL_INDEX)}).getFinalScore();
+    }
+
+    const secondRoll: number = totalRolls[CURRENT_FRAME_SECOND_ROLL_INDEX];
+
+    let frameScore: number = firstRoll + secondRoll;
+
+    if(isSpare(frameScore)){
+      frameScore += totalRolls[NEXT_FRAME_FIRST_ROLL_INDEX];
+      if(isLastFrame(totalRolls))  return frameScore;
+    }
+
+    return frameScore + createBowlingGame({rolls: totalRolls.slice(2)}).getFinalScore();
+  }
 })
 
-//const myOtherFunction = (...) => {...}
+const calculateStrikeFrameScore = (totalRolls: any[]) => { 
+  const nextTwoRolls = totalRolls.slice(CURRENT_FRAME_SECOND_ROLL_INDEX, NEXT_FRAME_SECOND_ROLL_INDEX);
+  
+  const nextTwoRollsScore = nextTwoRolls.reduce((nexTwoRollsScore, rollScore) => nexTwoRollsScore + rollScore, 0);
+
+  const strikeFrameScore =  STRIKE_ROLL_SCORE + nextTwoRollsScore;
+  
+  return strikeFrameScore;
+
+}
+
+const isSpare = (frameScore: number) => { return frameScore == 10 } 
+
+const isStrike = (firstRollScore: number) => { return firstRollScore == 10 } 
+
+
+
+
+function isLastFrame(totalRolls: any[]) {
+  return totalRolls.length == 3;
+}
+
