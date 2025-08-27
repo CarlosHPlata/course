4008daf35acf39cf7c19e47237010e4141e6fbb3
Carlos Plata <carlos.ksa21@gmail.com>
2025-08-28 00:45:50 +0200

    adding tdd for bowling game

--- inclassExamples/src/3_TDD/functional/bowlingGame.spec.ts
+++ inclassExamples/src/3_TDD/functional/bowlingGame.spec.ts
@@ -1,8 +1,61 @@
+import { createBowlingGame, Game } from "./bowlingGame";
 
 describe('Test bowling game', () => {
-
-  it('dummy test', () => {
-    expect(1).toBe(1)
+  let game: Game
+  beforeEach(() => {
+    game = createBowlingGame()
   })
 
+  test('all zeros', () => {
+    game = rollMany(20, 0);
+
+    expect(game.score()).toBe(0);
+  })
+
+  test('all ones', () => {
+    game = rollMany(20, 1);
+
+    expect(game.score()).toBe(20);
+  })
+
+  test('spare', () => {
+    game = rollSpare()
+    game = game.roll(3);
+    game = rollMany(17, 0);
+
+    expect(game.score()).toBe(16)
+  })
+
+  test('strike', () => {
+    game = rollStrike()
+    game = game.roll(3).roll(3)
+    game = rollMany(16, 0);
+
+    expect(game.score()).toBe(22)
+  })
+
+  test('all strikes', () => {
+    game = rollMany(12, 10);
+
+    expect(game.score()).toBe(300);
+  })
+
+
+  function rollStrike() {
+    return game.roll(10);
+  }
+
+  function rollSpare() {
+    return game.roll(5).roll(5)
+  }
+
+  function rollMany(rolls: number, pins: number) {
+    for (let i = 0; i < rolls; i++) {
+      game = game.roll(pins)
+    }
+
+    return game;
+  }
 });
+
+
--- inclassExamples/src/3_TDD/functional/bowlingGame.ts
+++ inclassExamples/src/3_TDD/functional/bowlingGame.ts
@@ -1,9 +1,65 @@
 
+export type Game = ReturnType<typeof createBowlingGame>
+type State = { rolls: number[] }
 
-type State = { /* put params if needed */ }
-const INITIAL_STATE: State = {}
+const INITIAL_STATE: State = { rolls: [] }
 
 export const createBowlingGame = (state: State = INITIAL_STATE) => Object.freeze({
-
+  roll: (pins: number) => roll(state, pins),
+  score: () => score(state),
 })
+
+
+
+
+function roll(state: State, pins: number) {
+  const newRolls = [...state.rolls, pins]; //rolls.add(pins)
+
+  return createBowlingGame({ rolls: newRolls })
+}
+
+function score({ rolls }: State) {
+  let score = 0;
+  const FRAMES = 10;
+  let firstRoll = 0
+
+  for (let frame = 0; frame < FRAMES; frame++) {
+    if (isStrike(rolls, firstRoll)) {
+      score += scoreForStrike(rolls, firstRoll)
+      firstRoll++
+
+    } else if (isSpare(rolls, firstRoll)) {
+      score += scoreForSpare(rolls, firstRoll)
+      firstRoll += 2
+
+    } else {
+      score += scoreForFrame(rolls, firstRoll)
+      firstRoll += 2
+    }
+  }
+
+  return score;
+}
+
+
+
+function scoreForStrike(rolls: number[], firstRoll: number) {
+  return 10 + rolls[firstRoll + 1] + rolls[firstRoll + 2];
+}
+
+function isStrike(rolls: number[], firstRoll: number) {
+  return rolls[firstRoll] === 10;
+}
+
+function scoreForFrame(rolls: number[], firstRoll: number) {
+  return rolls[firstRoll] + rolls[firstRoll + 1];
+}
+
+function scoreForSpare(rolls: number[], firstRoll: number) {
+  return 10 + rolls[firstRoll + 2];
+}
+
+function isSpare(rolls: number[], firstRoll: number) {
+  return rolls[firstRoll] + rolls[firstRoll + 1] == 10;
+}
 
